e1ef8d10182e4e8248ea01c8962256f8b84b65a8
Vasiliy Gladishev <gladishevvasiliy@gmail.com>
2018-12-25 13:14:20 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import Main from "../src/components/containers/Main/index";
+
+const App = () => (
+  <React.Fragment>
+    <h1>HELOOOOOOOOOOOOOOOO</h1>
+    <Main />
+  </React.Fragment>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ["babel-loader"]
+      },
+      { test: /\.tsx&$/, use: "ts-loader", exclude: /node_modules/ }
+    ]
+  },
+  resolve: { extensions: [".tsx", ".ts", ".js", ".jsx"] },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
